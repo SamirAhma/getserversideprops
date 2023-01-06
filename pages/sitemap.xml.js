f8bb30ea9aec167958f98d86b8a294c9ddd9f9ed
SamirAhma <30921483+SamirAhma@users.noreply.github.com>
2023-01-06 18:03:16 +0800

    add sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,56 +1,49 @@
-import React from "react";
-import * as fs from "fs";
-const Sitemap = () => {
-  return null;
-};
+//pages/sitemap.xml.js
+const EXTERNAL_DATA_URL = "https://jsonplaceholder.typicode.com/posts";
 
-export const getServerSideProps = async ({ res }) => {
-  const BASE_URL = "https://prodboost.netlify.app";
+function generateSiteMap(posts) {
+  return `<?xml version="1.0" encoding="UTF-8"?>
+   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+     <!--We manually set the two URLs we know already-->
+     <url>
+       <loc>https://jsonplaceholder.typicode.com</loc>
+     </url>
+     <url>
+       <loc>https://jsonplaceholder.typicode.com/guide</loc>
+     </url>
+     ${posts
+       .map(({ id }) => {
+         return `
+       <url>
+           <loc>${`${EXTERNAL_DATA_URL}/${id}`}</loc>
+       </url>
+     `;
+       })
+       .join("")}
+   </urlset>
+ `;
+}
 
-  const staticPaths = fs
-    .readdirSync("pages")
-    .filter((staticPage) => {
-      return ![
-        "api",
-        "_app.js",
-        "_document.js",
-        "404.js",
-        "sitemap.xml.js",
-      ].includes(staticPage);
-    })
-    .map((staticPagePath) => {
-      return `${BASE_URL}/${staticPagePath}`;
-    });
-  //   const blogPaths = fs.readdirSync("_posts").map((staticPagePath) => {
-  //     return `${BASE_URL}/posts/${staticPagePath.replace(".md", "")}`;
-  //   });
-  // , ...blogPaths
-  const allPaths = [...staticPaths];
-  console.log(allPaths);
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      ${allPaths
-        .map((url) => {
-          return `
-            <url>
-              <loc>${url}</loc>
-              <lastmod>${new Date().toISOString()}</lastmod>
-              <changefreq>monthly</changefreq>
-              <priority>1.0</priority>
-            </url>
-          `;
-        })
-        .join("")}
-    </urlset>
-`;
+function SiteMap() {
+  // getServerSideProps will do the heavy lifting
+}
+
+export async function getServerSideProps({ res }) {
+  // We make an API call to gather the URLs for our site
+  const request = await fetch(EXTERNAL_DATA_URL);
+  const posts = await request.json();
+
+  // We generate the XML sitemap with the posts data
+  const sitemap = generateSiteMap(posts);
 
   res.setHeader("Content-Type", "text/xml");
+  // we send the XML to the browser
   res.write(sitemap);
   res.end();
 
   return {
     props: {},
   };
-};
+}
 
-export default Sitemap;
+export default SiteMap;
